b63f759672519da5623e716e4307ed9c24e96a33
ErickLeoo <ericleonardo_2012@hotmail.com>
2025-06-08 22:59:29 -0300

    head componente for change titles and descriptions

--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -5,6 +5,7 @@ import useForm from '../../Hooks/useForm';
 import useFetch from '../../Hooks/useFetch';
 import { PASSWORD_RESET } from '../../api';
 import { useNavigate } from 'react-router-dom';
+import Head from '../Helper/Head';
 
 const LoginPasswordReset = () => {
   const [login, setLogin] = React.useState('');
@@ -36,6 +37,7 @@ const LoginPasswordReset = () => {
 
   return (
     <div>
+      <Head title="Reset a senha" />
       <h1 className="title">Resete a Senha</h1>
       <form onSubmit={handleSubmit}>
         <Input
